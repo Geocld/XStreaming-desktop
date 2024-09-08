70823a19bbf6cd27ccf38b13a0e8ea6b55e20591
Geocld <geocld@GeoclddeMacBook-Pro.local>
2024-09-08 11:14:14 +0800

    Update

--- src/pages/stream/Stream.tsx
+++ src/pages/stream/Stream.tsx
@@ -34,6 +34,42 @@ function Stream() {
         xHomeApi.waitState().then(() => {
           xHomeApi.getSessionConfig().then(configs => {
             console.log('configs:', configs);
+
+            xPlayer.createOffer().then((offer: any) => {
+              console.log('local offer:', offer);
+              xHomeApi.sendSdp(offer.sdp).then(() => {
+                xHomeApi.getSdp().then(res => {
+                  console.log('remote offer1:', res);
+
+                  const sdpDetails = JSON.parse(res.exchangeResponse);
+                  console.log('remote offer2:', sdpDetails);
+                  xPlayer.setRemoteOffer(sdpDetails.sdp);
+
+                  // Gather candidates
+                  const iceCandidates = xPlayer.getIceCandidates();
+                  const candidates = [];
+                  for(const candidate in iceCandidates) {
+                    candidates.push({
+                      candidate: iceCandidates[candidate].candidate,
+                      sdpMLineIndex: iceCandidates[candidate].sdpMLineIndex,
+                      sdpMid: iceCandidates[candidate].sdpMid,
+                    });
+                  }
+                  console.log('local ice:', candidates);
+                  xHomeApi.sendIce(candidates).then(() => {
+                    xHomeApi.getIce().then(res => {
+                      const exchangeIce = res.exchange_response;
+                      console.log('remote ice:', exchangeIce);
+                      xPlayer.setIceCandidates(exchangeIce);
+                      // Listen for connection change
+                      xPlayer.getEventBus().on('connectionstate', (event: any) => {
+                        console.log(':: Connection state updated:', event);
+                      });
+                    });
+                  });
+                });
+              });
+            });
           })
         })
       })
@@ -48,6 +84,7 @@ function Stream() {
     return () => {
       if(xPlayer !== undefined) {
         xPlayer.close();
+        setxPlayer(null);
       }
     };
   }, [xPlayer]);
